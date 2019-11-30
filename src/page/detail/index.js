028265f402d60d1590a4263b3aac9b66b9ab8474
dtvikey888 <dtvikey@163.com>
2019-11-30 20:25:43 +0800

    ok

--- src/page/detail/index.js
+++ src/page/detail/index.js
@@ -2,7 +2,7 @@
 * @Author: Dtvikey
 * @Date:   2019-11-11 20:38:27
 * @Last Modified by:   Dtvikey
-* @Last Modified time: 2019-11-30 20:24:27
+* @Last Modified time: 2019-11-30 20:25:39
 */
 
 'use strict';
@@ -49,7 +49,7 @@ var page = {
         this.loadNewList();
 
         // 增加点击次数
-        this.addCount();
+        //this.addCount();
     },
     bindEvent : function(){
         var _this = this;
