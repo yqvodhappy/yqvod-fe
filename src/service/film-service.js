b528a33caeacfa2c347ea5e1ef8670ed13f43c1c
dtvikey888 <dtvikey@163.com>
2020-04-29 16:30:55 +0800

    online test

--- src/service/film-service.js
+++ src/service/film-service.js
@@ -2,7 +2,7 @@
 * @Author: Dtvikey
 * @Date:   2019-11-09 09:32:43
 * @Last Modified by:   Dtvikey
-* @Last Modified time: 2019-11-30 20:20:55
+* @Last Modified time: 2020-04-28 20:09:50
 */
 
 'use strict';
@@ -13,7 +13,7 @@ var _film = {
     // 获取影片列表
     getFilmList : function(listParam, resolve, reject){
         _vv.request({
-            url     : _vv.getServerUrl('/film/list.do'),
+            url     : _vv.getServerUrl('/index/flist'),
             data    : listParam,
             success : resolve,
             error   : reject
@@ -22,7 +22,7 @@ var _film = {
     // 获取影片详细信息
     getFilmDetail : function(filmId, resolve, reject){
         _vv.request({
-            url     : _vv.getServerUrl('/film/detail.do'),
+            url     : _vv.getServerUrl('/index/getFilmDetailById'),
             data    : {
                 filmId : filmId
             },
@@ -33,7 +33,7 @@ var _film = {
     // 增加点击次数
     addCount : function(filmId, resolve, reject){
         _vv.request({
-            url     : _vv.getServerUrl('/film/take_count.do'),
+            url     : _vv.getServerUrl('/index/take_count'),
             data    : {
                 filmId : filmId
             },
