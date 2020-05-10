efa22b41d3774cddfec164320e1dff0bced4f3dc
dtvikey888 <dtvikey@163.com>
2020-05-10 10:16:51 +0800

    ok

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -2,7 +2,7 @@
 * @Author: Dtvikey
 * @Date:   2019-11-09 09:21:25
 * @Last Modified by:   Dtvikey
-* @Last Modified time: 2020-05-10 10:05:09
+* @Last Modified time: 2020-05-10 10:12:03
 */
 
 'use strict';
@@ -73,7 +73,7 @@ var page = {
             listHtml    = '',
             listParam   = this.data.listParam,
             $pListCon   = $('.p-list-con');
-        $pListCon.html('999');
+        $pListCon.html('<div class="loading"></div>');
         // 删除参数中不必要的字段
         listParam.categoryId 
             ? (delete listParam.keyword) : (delete listParam.categoryId);
